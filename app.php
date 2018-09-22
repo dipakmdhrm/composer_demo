0281f24eca444698bb5303cd2baf57df1518444a
Dipak Yadav <dipakmdhrm@gmail.com>
2018-09-22 19:35:36 +0530

    Used guzzle to call an api.

--- app.php
+++ app.php
@@ -4,6 +4,20 @@ namespace ComposerDemo;
 require_once "vendor/autoload.php";
 
 use Rivsen\Demo\Hello;
+use GuzzleHttp\Client;
 
+// Use rivsen/hello-world to print Hello.
 $hello = new Hello('Dipak');
-echo $hello->hello();
+echo $hello->hello() . PHP_EOL;
+
+// Use guzzlehttp/guzzle to call an API.
+$client = new Client();
+$response = $client->request(
+  'GET',
+  'https://jsonplaceholder.typicode.com/posts'
+);
+echo $response->getStatusCode() . PHP_EOL;
+// 200
+echo $response->getHeaderLine('content-type') . PHP_EOL;
+// 'application/json; charset=utf8'
+echo $response->getBody() . PHP_EOL;
